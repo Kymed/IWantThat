ac459368830e37bc5eded0848d62c1776b6f6cef
Kyle Meade <Kyle.meade37@gmail.com>
2019-02-02 11:49:15 -0500

    Moved things to controller.
    Gonna start making the get/post/redirect cycle when Fang is finished his upload image functionality. And theyre done their HTML files.

--- controllers/visionController.js
+++ controllers/visionController.js
@@ -1,7 +1,35 @@
-var multer = require('multer');
-var upload = multer({ dest:'../public/images' })
+var path = require('path');
+var vision = vision = require('@google-cloud/vision');
+var request = require('request');
+var visionClient = new vision.ImageAnnotatorClient();
 
-exports.detectAnimal = (req, res) => {
-    
 
+exports.detectAnimal = (imgPath) => {
+    const image = imgPath;
+    visionClient.labelDetection(imgPath).then(results => {
+
+        const labels=results[0].labelAnnotations;
+        let ignoreLabels = ["Companion Dog", "Carnivore", "Snout", "Canidae",
+        "Dog breed", "Vertebrate", "Mammal", "Canidae", "Black", "Maroon", "Green", "Olive",
+        "Navy", "Purple", "Teal", "Silver", "Gray", "Red", "Lime", "Yellow",
+        "Blue", "Fuchsia", "Aqua", "White", "Ancient dog breeds"]
+
+        let labelNames = [];
+
+        labels.forEach(label => {
+
+            if (!ignoreLabels.includes(label.description)) {
+                labelNames.push(label.description);
+            };
+
+        });
+        
+        request('/chooseLabel', function (err, res, body) {
+            if(err) {console.log(err);};
+        })
+    }).catch(err => {
+        console.error("ERROR: ", err);
+    })
 }
+
+exports.req
--- routes.js
+++
@@ -1,5 +0,0 @@
-const multer = require('multer');
-
-const HandleError = (req, res) => {
-    res.status(500).contentType("text.plain").end("Something went wrong");
-}
