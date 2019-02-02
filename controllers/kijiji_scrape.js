d7b78e6a2c0fbfcfeee98ca1435197ef912f9b2d
吴方宇 <wufangyu@wufangyudeMacBook-Pro.local>
2019-02-02 11:28:24 -0500

    changed the ads type to OFFER

--- controllers/kijiji_scrape.js
+++ controllers/kijiji_scrape.js
@@ -1,12 +1,12 @@
 const kijiji = require("kijiji-scraper");
 
 let options = {
-    minResults: 5,
+    minResults: 6,
     scrapeResultDetails: true,
 
 };
 
-/*
+
 const pet_Category = 112;
 const Kingston = 1700181;
 const Guelph = 1700184;
@@ -16,7 +16,7 @@ const Ottawa_Gatineau_Region = 1700184;
 const St_Catherines = 80016;
 const Peterborough_Area = 1700217;
 const Toronto_GTA = 1700272;
-*/
+
  
 let params = {
     locationId: Toronto_GTA,  // Same as kijiji.locations.ONTARIO.OTTAWA_GATINEAU_AREA.OTTAWA
@@ -32,18 +32,18 @@ function callback(err, ads) {
     if (!err) {
         // Use the ads array https://www.kijiji.ca/v-cars-trucks/ottawa/lease-take-over-mercedes-benz-cla/1411235322
         for (let i = 0; i < ads.length; ++i) {
-
-        	var res = {
-        		"title": ads[i].title,
-        		"url" : ads[i].url,
-        		"price":  ads[i].attributes["price"],
-        		"images": ads[i].images,
-
-        	}
-
-            console.log(res);
+            if(ads[i].attributes["type"] == "OFFER"){
+                var res = {
+                "title": ads[i].title,
+                "url" : ads[i].url,
+                "price":  ads[i].attributes["price"],
+                "images": ads[i].images,
+                }
+                console.log(res);
+            }
         }
     }
 }
 kijiji.search(params, options, callback);
+
 
