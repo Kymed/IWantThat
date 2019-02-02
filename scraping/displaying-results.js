95da0d650ff82f708703f8347cd74c0608c9926c
TobCar <TobCar@users.noreply.github.com>
2019-02-01 23:32:17 -0500

    Add functions to create cards for each ad

--- scraping/displaying-results.js
+++ scraping/displaying-results.js
@@ -13,6 +13,40 @@ function createAdCard(ad) {
     "class": "card-body"
   })
   $adCard.append($adCardBody);
+  $adCard.append(createAdCardImages(ad));
+  $adCardBody.append(createAdCardContent(ad));
 
   return $adCard;
+}
+
+function createAdCardImages(ad) {
+  let $image = $("<img>", {
+    "class": "card-img-top",
+    src: ad.image
+  })
+
+  return image;
+}
+
+function createAdCardContent(ad) {
+  let $title = $("<h4>", {
+    "class": "card-title",
+    html: ad.title
+  })
+
+  let $description = $("<p>", {
+    "class": "card-text",
+    html: ad.description
+  })
+
+  let $date = $("<p>", {
+    "class": "small",
+    html: ad.date
+  })
+
+  let $seePost = $("<a>", {
+    href: ad.url
+  })
+
+  return [$title, $description, $date, $seePost]
 }
