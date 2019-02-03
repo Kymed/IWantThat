2a8e91dfe4d57e8f9b456351d70d74095aa1bfa5
TobCar <TobCar@users.noreply.github.com>
2019-02-03 00:42:16 -0500

    Expand labels to exclude

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.post('/uploadImage', upload.single('image'), (req, res) => {
         "Navy", "Purple", "Teal", "Silver", "Gray", "Red", "Lime", "Yellow",
         "Blue", "Fuchsia", "Aqua", "White", "Ancient dog breeds", "Dog", "Cat", "Felidae",
         "Small to medium-sized cats", "Kitten", "Whiskers", "Domestic short-haired cat", "Grass", "Eye",
-        "Whiskers", "Grass", "Wildlife", "Terrestrial Animal"];
+        "Whiskers", "Grass", "Wildlife", "Terrestrial Animal", "Nose", "Eye", "Animal"];
 
         labels.forEach(label => {
 
@@ -90,7 +90,7 @@ app.post('/uploadImage', upload.single('image'), (req, res) => {
         if (err) {
             console.log("failed to delete local image:"+err);
         } else {
-            console.log('successfully deleted local image');                                
+            console.log('successfully deleted local image');
         }
     });
 
